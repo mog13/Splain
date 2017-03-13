559220578102bb9eea421d6eb5f2c7be9f75f6b8
Morgan Owen <mog13@live.com>
2017-03-13 19:46:58 +0000

    added parameter parsing and creation of an execution array

--- spec/basic.spec.js
+++ spec/basic.spec.js
@@ -1,14 +1,35 @@
 var Splain = require('../splain.js');
 
 
-describe("when using the parser", function () {
+describe("when using the parser", () => {
     let splain, input;
-    beforeEach(()=>{
-        input = "this is a {qual}{adj} test.";
+    beforeEach(() => {
+        input = "this is a {quantitive}{adj} test.";
         splain = new Splain();
     });
 
-    it("should be able to find splain groups", function () {
-        expect(splain.$findGroups().length).toBe(2);
-    })
+    it("should be able to trim the {} from the splain group", () => {
+       expect(splain.$trimGroup("{this is a test}")).toBe("this is a test");
+    });
+
+    it("should be able to find splain groups", () => {
+        expect(splain.$findGroups(input).length).toBe(2);
+    });
+
+    it("should be able to find inputs in a group", () => {
+        let inputs = splain.$findInputs('input1 input2?|"test?"?');
+        expect(inputs[0]).toBe("input1");
+        expect(inputs[2]).toBe('"test?"');
+        expect(inputs.length).toBe(3)
+    });
+
+    it("should be able to break it up into an array or variables and executions", () => {
+       let execArray = splain.$getExecutionArray('input1 input2|"test"?');
+        expect(execArray[0]).toBe("input1");
+        expect(execArray[1]).toBe("input2");
+        expect(execArray[2]).toBe("|");
+        expect(execArray[3]).toBe('"test"');
+        expect(execArray[4]).toBe("?");
+    });
+
 });
--- splain.js
+++ splain.js
@@ -1,11 +1,44 @@
 
 class Splain {
     constructor() {
-
+        this.regex = {
+            groupSelector: /\{.+?\}/g,
+            chanceSelector: /\?[1-9]?(?=[\| ])/g,
+        }
+    }
+    $trimGroup(input){
+        return input.substring(1,input.length-1);
     }
 
-    $findGroups(){
+    $findGroups(input){
+        return input.match(this.regex.groupSelector);
+    }
 
+    $findInputs(input) {
+        input = input.replace(/[\?|](?=([^"]*"[^"]*")*[^"]*$)/g," ");
+        return (input).split(" ").filter(function(n){
+            return n != "" }); ;
+    }
+
+    $getExecutionArray(input) {
+        let inputs = this.$findInputs(input),
+            execs = [];
+        while(input.length>0) {
+            // if there arnt inputs or the input is the next part
+            if (input[0] == ' '){
+                input = input.slice(1);
+            }
+            else if (inputs.length == 0  || input.indexOf(inputs[0]) > 0) {
+                execs.push(input[0]);
+                input = input.slice(1);
+            }
+            else {
+                execs.push(inputs[0]);
+                input = input.replace(inputs[0], "");
+                inputs.shift();
+            }
+        }
+        return execs;
     }
 
 }
