7f812435dca5083f441d864df3f9fd77fcc6b551
Morgan Owen <mog13@live.com>
2017-08-20 09:52:13 +0100

    added initial webpack config with babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './lib/index.js',
+    output: {
+        filename: 'splain.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+}
