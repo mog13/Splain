99c2fd08505c7eb78d2ca93c6f84ccdfc88a9439
Morgan Owen <mog13@live.com>
2020-02-10 21:10:48 +0000

    added contextMatcher and token executor

--- src/Utility/TokenExecuter.ts
+++ src/Utility/TokenExecuter.ts
@@ -1,11 +1,27 @@
 import {Token} from "../Token";
 import {Dictionary} from "../Dictionary";
 import {ProcessInfo} from "../Processor";
+import {Entry} from "../Entry";
+import {matchContexts} from "./ContextMatcher";
 
 export function executeToken(token:Token, dictionary:Dictionary, processInfo:ProcessInfo):any {
 
     const choices = dictionary.getEntries(token.value);
+    if(!choices) return null;
 
+    const weightedChoices: Entry[] = [];
+    // we are doing it this way around to preserve weightings and then cut down contexts.
+    // doing the reverse would give weightings that weren't balanced with applicable choices.
+    choices.forEach((choice:Entry)=>{
+       for(let n =0;n <choice.computedWeight *choice.weight; n++) {
+           weightedChoices.push(choice)
+       }
+    });
 
-    return null;
+    const contextualChoices: Entry[] = weightedChoices.filter((entry)=>{
+        return matchContexts(entry,processInfo.contexts);
+    });
+
+    if(contextualChoices.length ===0) return null;
+    return contextualChoices[Math.floor(Math.random()* contextualChoices.length)];
 }
